500da78cf3ab383a575b94da73305f372e66da32
Russ Lightsey <texanruss3@gmail.com>
2021-07-21 22:38:59 -0700

    Implemented ReviewBreakdown and filter method

---
+++ client/components/ratings/ReviewBreakdown.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+
+class ReviewBreakdown extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filteredStars: []
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(star) {
+    let index = this.state.filteredStars.indexOf(star);
+    if (index === -1) {
+      this.state.filteredStars.push(star);
+      this.props.handleFilter(star);
+    } else {
+      this.state.filteredStars.splice(index, 1);
+      this.props.handleRemove(star);
+    }
+  }
+
+
+  render() {
+    const recommendTrue = Number(this.props.meta.recommended.true);
+    const recommendFalse = Number(this.props.meta.recommended.false);
+    const recommendPercent = Math.floor(recommendTrue / (recommendTrue + recommendFalse) * 100);
+    return (
+      <div>
+        <h3>Ratings & Reviews</h3>
+        <p>{recommendPercent}% of reviews recommend this product</p>
+        <table border="1px">
+          <tbody>
+            <tr>
+              <td onClick={() => this.handleClick(5)}>5 stars</td>
+              <td>percent</td>
+            </tr>
+            <tr>
+              <td onClick={() => this.handleClick(4)}>4 stars</td>
+              <td>percent</td>
+            </tr>
+            <tr>
+              <td onClick={() => this.handleClick(3)}>3 stars</td>
+              <td>percent</td>
+            </tr>
+            <tr>
+              <td onClick={() => this.handleClick(2)}>2 stars</td>
+              <td>percent</td>
+            </tr>
+            <tr>
+              <td onClick={() => this.handleClick(1)}>1 stars</td>
+              <td>percent</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default ReviewBreakdown;
--- client/components/ratings/ReviewList.jsx
+++ client/components/ratings/ReviewList.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import App from '../App.jsx';
 import ReviewItem from './ReviewItem.jsx';
+import ReviewBreakdown from './ReviewBreakdown.jsx';
 
 class ReviewList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      reviews: [],
       view: 2
     }
+    this.filterByRating = this.filterByRating.bind(this);
+    this.removeFilter = this.removeFilter.bind(this);
+    this.removeAll = this.removeAll.bind(this);
   }
   //METHODS
+  filterByRating(star) {
+    let filter = this.props.reviews.filter((item) => item.rating === star);
+    this.setState({reviews: filter.concat(this.state.reviews), view: this.state.reviews.length});
+  }
+
+  removeFilter(star) {
+    let remove = this.state.reviews.filter((item) => item.rating !== star);
+    this.setState({reviews: remove, view: remove.length}, () => {
+      if (this.state.reviews.length === 0) {
+        this.setState({view: 2});
+      }
+    });
+  }
+
+  removeAll() {
+    this.setState({reviews: [], view: 2});
+  }
 
   render() {
     const list = this.props.reviews.slice(0, this.state.view).map((item) =>
@@ -29,10 +51,31 @@ class ReviewList extends React.Component {
           <p></p>
       </div>
     );
+    const filterList = this.state.reviews.map((item) =>
+    <div key={item.review_id}>
+        <ReviewItem
+          key={item.review_id}
+          rating={item.rating}
+          summary={item.summary}
+          recommend={item.recommend}
+          response={item.response}
+          body={item.body}
+          date={item.date}
+          author={item.reviewer_name}
+          helpfulness={item.helpfulness}
+          photos={item.photos}
+          />
+        <p></p>
+    </div>
+  );
     return (
       <div>
-        {list}
-        <button onClick={() => this.setState({view: this.state.view += 2})}>More Reviews</button>
+        <ReviewBreakdown meta={this.props.meta} handleFilter={this.filterByRating} handleRemove={this.removeFilter} handleRemoveAll={this.removeAll}/>
+        {this.state.reviews.length === 0 ? list : filterList}
+        <div>
+          {this.state.reviews.length === 0 && <button onClick={() => this.setState({view: this.state.view += 2})}>More Reviews</button>}
+          <button>Add a Review +</button>
+        </div>
       </div>
     );
   }
