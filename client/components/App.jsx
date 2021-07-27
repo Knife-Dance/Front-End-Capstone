af7dec2022d80a93667e58f956ae97883f34f8f9
Russ Lightsey <texanruss3@gmail.com>
2021-07-27 09:11:13 -0700

    first push after master branch merge

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Overview from './OverviewComponent/Overview.jsx';
 import Ratings from './Ratings.jsx';
-<<<<<<< HEAD
-//import ReviewList from './ratings/ReviewList.jsx';
-
-=======
-import ReviewList from './ratings/ReviewList.jsx';
+import ReviewList from './ratings/ReviewList/ReviewList.jsx';
 import Relate from './related/product-card/Related-product.jsx'
 import dataUrl from './related/sample-data.js'
->>>>>>> 635ff15185b1ece6719f81976962f3b97fa3d896
 import { sampleData } from '../sampleData.js';
 import { metaData } from '../metaData.js';
 
@@ -30,7 +25,7 @@ class App extends React.Component{
         {/* <Overview /> */}
         <Relate cards={dataUrl}/>
 
-        {/* <ReviewList reviews={this.state.reviews} meta={this.state.meta} /> */}
+        <Ratings reviews={this.state.reviews} meta={this.state.meta} />
       </div>
     );
   }
