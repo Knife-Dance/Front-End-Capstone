0d72d06d91a03e8e760550c7ee114ded76a73add
Zadok Kim <zadokjhkim@gmail.com>
2021-07-27 09:42:29 -0700

    after merge with master

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Overview from './OverviewComponent/Overview.jsx';
 import Ratings from './Ratings.jsx';
 import ReviewList from './ratings/ReviewList.jsx';
-import Relate from './related/product-card/Related-product.jsx'
-import dataUrl from './related/sample-data.js'
+import Relate from './related/product-card/Related-product.jsx';
+import dataUrl from './related/sample-data.js';
 import { sampleData } from '../sampleData.js';
 import { metaData } from '../metaData.js';
 
@@ -23,7 +23,7 @@ class App extends React.Component{
     return (
       <div>
         {/* <h1>THIS IS KNIFE DANCE!</h1> */}
-        {/* <Overview /> */}
+        <Overview />
         <Relate cards={dataUrl}/>
 
         {/* <ReviewList reviews={this.state.reviews} meta={this.state.meta} /> */}
