fba4ec73466f271a7a585fec72964908b902998f
Zadok Kim <zadokjhkim@gmail.com>
2021-08-06 10:10:06 -0700

    merge purposes

--- client/components/shared/context/Provider.jsx
+++ client/components/shared/context/Provider.jsx
@@ -27,48 +27,12 @@ const Provider = ({ children }) => {
           {
             "thumbnail_url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
             "url": "https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80"
-          },
-          {
-            "thumbnail_url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "url": "https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"
-          },
-          {
-            "thumbnail_url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-            "url": "https://images.unsplash.com/photo-1527522883525-97119bfce82d?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
-          },
-          {
-            "thumbnail_url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "url": "https://images.unsplash.com/photo-1556648202-80e751c133da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-          },
-          {
-            "thumbnail_url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "url": "https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
           }
         ],
         "skus": {
           "522040": {
             "quantity": 8,
             "size": "XS"
-          },
-          "522041": {
-            "quantity": 16,
-            "size": "S"
-          },
-          "522042": {
-            "quantity": 17,
-            "size": "M"
-          },
-          "522043": {
-            "quantity": 10,
-            "size": "L"
-          },
-          "522044": {
-            "quantity": 15,
-            "size": "XL"
-          },
-          "522045": {
-            "quantity": 4,
-            "size": "XL"
           }
         }
       }
@@ -139,26 +103,6 @@ const Provider = ({ children }) => {
       "522040": {
         "quantity": 8,
         "size": "XS"
-      },
-      "522041": {
-        "quantity": 16,
-        "size": "S"
-      },
-      "522042": {
-        "quantity": 17,
-        "size": "M"
-      },
-      "522043": {
-        "quantity": 10,
-        "size": "L"
-      },
-      "522044": {
-        "quantity": 15,
-        "size": "XL"
-      },
-      "522045": {
-        "quantity": 4,
-        "size": "XL"
       }
     }
   });
